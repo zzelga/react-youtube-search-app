e587b53ce882c656b1ad5ff957ef12586dfa405f
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2016-03-11 15:18:15 -0500

    Set up minimal code for SearchBar component, render to browser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { YOUTUBE_DATA_API_KEY } from '../secrets';
+import SearchBar from './components/search_bar';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#target'));
