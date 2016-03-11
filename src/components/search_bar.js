a47073ba1d87f0869f1c8e42b2901152e928390b
Zbigniew Zelga <zbigniew.zelga@gmail.com>
2016-03-11 16:43:26 -0500

    Refactor SearchBar into a controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,13 @@ class SearchBar extends Component {
   }
 
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input
+          value={this.state.searchTerm}
+          onChange={event => this.setState({ searchTerm: event.target.value })} />
+      </div>
+    );
   }
 }
 
